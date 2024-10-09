458f64a1b31d9ad41fd0e49a5bf16654429f0251
Peder Linder <pederbl@gmail.com>
2024-10-09 14:15:31 +0200

    add start command

---
+++ commands/start.go
@@ -0,0 +1,44 @@
+package commands
+
+import (
+	"fmt"
+	"cillers-cli/lib"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"strings"
+)
+
+type Config struct {
+	Stack []string `yaml:"stack"`
+}
+
+func CommandStart(args []string, options map[string]bool) error {
+	verbose := options["verbose"]
+	if verbose {
+		fmt.Println("Starting Cillers ...")
+	}
+
+	configFile, err := ioutil.ReadFile("./conf/cillers.yml")
+	if err != nil {
+		return fmt.Errorf("error reading config file: %v", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		return fmt.Errorf("error parsing config file: %v", err)
+	}
+
+	if len(config.Stack) == 0 {
+		return fmt.Errorf("no stack items found in the configuration")
+	}
+
+	command := "pt"
+	args = append([]string{"pt", "run"}, config.Stack...)
+
+	if verbose {
+		fmt.Printf("Executing command: %s %s\n", command, strings.Join(args[1:], " "))
+	}
+
+	return lib.ExecuteTakeOverCurrentProcess(command, args)
+}
